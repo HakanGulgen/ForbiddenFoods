f01ab35554856ea56c0e0fb0d54c091addffadd7
Hakan <HakanGulgen@users.noreply.github.com>
2017-06-13 15:43:58 +0300

    Initial commit

---
+++ src/main/java/io/github/hakangulgen/forbiddenfoods/ForbiddenFoodsPlugin.java
@@ -0,0 +1,17 @@
+package io.github.hakangulgen.forbiddenfoods;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ForbiddenFoodsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
